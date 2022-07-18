6bfe74d37d4f05ded699fadc7fec328ddad1e3b5
zhuxuechang <1494135711@qq.com>
2022-07-18 17:03:21 +0800

    first commit

---
+++ prmise.js
@@ -0,0 +1,26 @@
+function Promise(fn) {
+  // 存储回调函数
+  this.cbs = [];
+
+  const resolve = (value) => {
+    setTimeout(() => {
+      this.data = value;
+      this.cbs.forEach((cb) => cb(value));
+    });
+  }
+
+  fn(resolve);
+}
+
+Promise.prototype.then = function (onResolved) {
+  return new Promise((resolve) => {
+    this.cbs.push(() => {
+      const res = onResolved(this.data);
+      if (res instanceof Promise) {
+        res.then(resolve);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+};
