7b2827a22d5083edd8fbd9b3434f53ad853778d3
zhuxuechang <1494135711@qq.com>
2022-07-19 11:00:18 +0800

    feat: then & catch

--- promise.js
+++ promise.js
@@ -31,3 +31,66 @@ function Promise(executor) {
     reject(e);
   }
 }
+
+Promise.prototype.then = function (onResolved, onRejected) {
+  let self = this;
+  let promise2;
+
+  onResolved = typeof onResolved === 'function' ? onResolved : function () {};
+  onRejected = typeof onRejected === 'function' ? onRejected : function () {};
+
+  if(self.status === 'resolved') {
+    return promise2 = new Promise(function (resolve, reject) {
+      try {
+        let p = onResolved(self.data);
+        if(p instanceof Promise) {
+          p.then(resolve, reject);
+        }
+        resolve(p)
+      } catch (e) {
+        reject(e);
+      }
+    })
+  }
+  if(self.status === 'rejected') {
+    return promise2 = new Promise(function (resolve, reject) {
+      try {
+        let p = onRejected(self.data);
+        if(p instanceof Promise) {
+          p.then(resolve, reject)
+        }
+        reject(p)
+      } catch (e) {
+        reject(e)
+      }
+    })
+  }
+  if(self.status === 'pending') {
+    return promise2 = new Promise(function (resolve, reject) {
+      self.onResolvedCallback.push(function (value) {
+        try {
+          let p = resolve(value)
+          if(p instanceof Promise){
+            p.then(resolve, reject)
+          }
+        } catch (e) {
+          reject(e)
+        }
+      })
+      self.onRejectedCallback.push(function(reason) {
+        try {
+          let p = reject(reason);
+          if(p instanceof Promise) {
+            p.then(resolve, reject)
+          }
+        } catch (e) {
+          reject(e)
+        }
+      })
+    })
+  }
+}
+
+Promise.prototype.catch =  function (onRejected) {
+  return this.then(null, onRejected)
+}
