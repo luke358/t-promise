601da87b54ffea07413334f9b6832d41668922c7
zhuxuechang <1494135711@qq.com>
2022-07-19 14:04:02 +0800

    feat: resolvePromise --DRY

--- promise.js
+++ promise.js
@@ -6,23 +6,30 @@ function Promise(executor) {
   self.onRejectedCallback = [];
 
   function resolve(value) {
-    if(self.status === 'pending') {
-      self.status = 'resolved';
-      self.data = value;
-      self.onResolvedCallback.forEach(fn => {
-        fn(value);
-      })
+    if (value instanceof Promise) {
+      return value.then(resolve, reject)
     }
+    setTimeout(function () {
+      if(self.status === 'pending') {
+        self.status = 'resolved';
+        self.data = value;
+        self.onResolvedCallback.forEach(fn => {
+          fn(value);
+        })
+      }
+    })
   }
 
   function reject(reason) {
-    if(self.status === 'pending') {
-      self.status = 'rejected';
-      self.data = reason;
-      self.onRejectedCallback.forEach(fn => {
-        fn(reason);
-      })
-    }
+    setTimeout(() => {
+      if(self.status === 'pending') {
+        self.status = 'rejected';
+        self.data = reason;
+        self.onRejectedCallback.forEach(fn => {
+          fn(reason);
+        })
+      }
+    });
   }
 
   try {
@@ -43,10 +50,7 @@ Promise.prototype.then = function (onResolved, onRejected) {
     return promise2 = new Promise(function (resolve, reject) {
       try {
         let p = onResolved(self.data);
-        if(p instanceof Promise) {
-          p.then(resolve, reject);
-        }
-        resolve(p)
+        resolvePromise(promise2, p, resolve, reject);
       } catch (e) {
         reject(e);
       }
@@ -56,10 +60,7 @@ Promise.prototype.then = function (onResolved, onRejected) {
     return promise2 = new Promise(function (resolve, reject) {
       try {
         let p = onRejected(self.data);
-        if(p instanceof Promise) {
-          p.then(resolve, reject)
-        }
-        reject(p)
+        resolvePromise(promise2, p, resolve, reject);
       } catch (e) {
         reject(e)
       }
@@ -69,25 +70,67 @@ Promise.prototype.then = function (onResolved, onRejected) {
     return promise2 = new Promise(function (resolve, reject) {
       self.onResolvedCallback.push(function (value) {
         try {
-          let p = resolve(value)
-          if(p instanceof Promise){
-            p.then(resolve, reject)
-          }
+          let p = onResolved(value)
+          resolvePromise(promise2, p, resolve, reject);
         } catch (e) {
           reject(e)
         }
       })
       self.onRejectedCallback.push(function(reason) {
         try {
-          let p = reject(reason);
-          if(p instanceof Promise) {
-            p.then(resolve, reject)
-          }
+          let p = onRejected(reason);
+          resolvePromise(promise2, p, resolve, reject);
         } catch (e) {
           reject(e)
         }
       })
     })
+  }
+}
+
+function resolvePromise(promise2, p, resolve, reject) {
+  var then
+  var thenCalledOrThrow = false
+
+  // 不能自己引用自己
+  if (promise2 === p) {
+    return reject(new TypeError('Chaining cycle detected for promise!'))
+  }
+
+  if (p instanceof Promise) {
+    if (p.status === 'pending') { //because p could resolved by a Promise Object
+      p.then(function(v) {
+        resolvePromise(promise2, v, resolve, reject)
+      }, reject)
+    } else { //but if it is resolved, it will never resolved by a Promise Object but a static value;
+      p.then(resolve, reject)
+    }
+    return
+  }
+
+  if ((p !== null) && ((typeof p === 'object') || (typeof p === 'function'))) {
+    try {
+      then = p.then //because p.then could be a getter
+      if (typeof then === 'function') {
+        then.call(p, function rs(y) {
+          if (thenCalledOrThrow) return
+          thenCalledOrThrow = true
+          return resolvePromise(promise2, y, resolve, reject)
+        }, function rj(r) {
+          if (thenCalledOrThrow) return
+          thenCalledOrThrow = true
+          return reject(r)
+        })
+      } else {
+        resolve(p)
+      }
+    } catch (e) {
+      if (thenCalledOrThrow) return
+      thenCalledOrThrow = true
+      return reject(e)
+    }
+  } else {
+    resolve(p)
   }
 }
 
